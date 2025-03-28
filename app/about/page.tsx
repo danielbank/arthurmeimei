1e9cf22858fc45a39d1aaaab89a57963c7e6a1c8
Daniel Bank <daniel@xaac.com>
2025-03-28 14:46:18 -0700

    Logo, authors, and content edits

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,15 +6,18 @@ import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
-export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
+function renderAuthor(slug: string) {
+  const author = allAuthors.find((p) => p.slug === slug) as Authors
   const mainContent = coreContent(author)
-
   return (
-    <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
-    </>
+    <AuthorLayout content={mainContent}>
+      <MDXLayoutRenderer code={author.body.code} />
+    </AuthorLayout>
   )
+}
+
+export default function Page() {
+  const authors = allAuthors
+
+  return <>{authors.map((author) => renderAuthor(author.slug))}</>
 }
